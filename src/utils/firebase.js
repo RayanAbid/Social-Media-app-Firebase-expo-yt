6ba580edbd06eae2ea18067dc78108f39f647a1f
Rayan Abid <68749736+RayanAbid@users.noreply.github.com>
2023-03-20 14:50:04 +0500

    Update firebase.js

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -3,19 +3,19 @@ import { initializeApp } from "firebase/app";
 
 import { getAuth } from "firebase/auth";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// TODO:YOUR_KEY,
+// https:YOUR_KEY,
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDiMbkjJW8PPDBJEuUtnhnhDmt1t3i6C2Y",
-  authDomain: "social-media-d43d2.firebaseapp.com",
-  projectId: "social-media-d43d2",
-  storageBucket: "social-media-d43d2.appspot.com",
-  messagingSenderId: "225537058077",
-  appId: "1:225537058077:web:c2ab09463e5df72189edf4",
-  measurementId: "G-DW6PT9TH5V",
+  apiKey: YOUR_KEY,
+  authDomain: YOUR_KEY,
+  projectId: YOUR_KEY,
+  storageBucket: YOUR_KEY,
+  messagingSenderId: YOUR_KEY,
+  appId: YOUR_KEY,
+  measurementId: YOUR_KEY,
 };
 
 // Initialize Firebase
